d07da429ff2b32cc56805746ab1b4194bc88ee85
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-26 12:01:00 +0200

    Add test checking enqueue() behaviour when null passed

--- src/test/java/com/codecool/QueueTest.java
+++ src/test/java/com/codecool/QueueTest.java
@@ -1,7 +1,17 @@
 package com.codecool;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueTest {
+
+    @Test
+    void throwsException_when_nullPassed() {
+        CustomQueue queue = new CustomQueue();
+        assertThrows(IllegalArgumentException.class, () -> {
+            queue.enqueue(null);
+        });
+    }
 
 }
