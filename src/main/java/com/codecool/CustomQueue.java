1c1d6a0cb37269b739d6361fe970bec74567b47b
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-26 11:36:09 +0200

    Add methods signatures

--- src/main/java/com/codecool/CustomQueue.java
+++ src/main/java/com/codecool/CustomQueue.java
@@ -5,8 +5,20 @@ public class CustomQueue {
     private Node head;
     private Node tail;
 
-    public CustomQueue(String value) {
+    public CustomQueue() {
         head = null;
         tail = null;
+    }
+
+    public void enqueue(String value) {
+
+    }
+
+    public String peek() {
+        return null;
+    }
+
+    public String dequeue() {
+        return null;
     }
 }
